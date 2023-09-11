728a476af2a3658cb7911ffb7cf29cc0a1fad12f
marc@noops.rocks <marc@noops.rocks>
2023-09-11 13:41:58 +0200

    add params adder template

---
+++ rust/add_return_200/src/lib.rs
@@ -0,0 +1,25 @@
+wit_bindgen::generate!({
+    world: "handler",
+    exports: {
+        world: TestHandler
+    }
+});
+
+struct TestHandler;
+
+impl Guest for TestHandler {
+    fn handle(req: Request) -> Response {
+        let mut response_body = String::default();
+        let mut result = 0;
+
+        for (_, value) in req.query_params {
+            result += value.parse::<i32>().expect("NaN");
+        }
+        response_body = result.to_string();
+        response_body.push_str("\n");
+        Response {
+            status: 200,
+            body: response_body,
+        }
+    }
+}
